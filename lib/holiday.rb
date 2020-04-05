630c1c928c824dcdeef3ff2937cde4e627b39a95
Evan Smith <professionalemail@zoho.com>
2020-04-05 20:23:45 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,51 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  
+  holiday_hash.each do |season, holiday|
+    if season == :winter
+      holiday.each do |day, item|
+        item << supply
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  
+  # Not sure if there's an easier way to do this since there's only one spring holiday ...
+  
+  holiday_hash.each do |season, holiday|
+    if season == :spring
+      holiday.each do |day, item|
+        item << supply
+      end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  
+  # Is this right? Did I cheat? Haha
+  
+holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,6 +73,23 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  seasons=[]
+  holiday_hash.keys.each do |season|
+  seasons << season.to_s.capitalize
+  end
+  
+  holidays=[]
+  holiday_hash.each do |season, day|
+    day.each do |specific_day, item|
+    holidays << specific_day.to_s.capitalize
+    end
+  end
+  
+  holidays_cap=[]
+  holidays.each do |check_h|
+    
+  end
 
 end
 
